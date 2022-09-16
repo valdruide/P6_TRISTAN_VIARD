63fa3fce21db1f9d657e8a5d41e86ff247ab0d39
Valdruide <tviardprimo190@gmail.com>
2022-09-16 16:13:10 +0200

    init

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const stuffRoutes = require('./routes/stuff')
+const userRoutes = require('./routes/user')
+
+mongoose.connect('mongodb+srv://tristan:admin@cluster0.4mrastv.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')))
+
+
+module.exports = app;
